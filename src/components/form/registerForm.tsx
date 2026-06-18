7ea0b2fd2aacd4065baf978090b69847071d5f37
Erick dps <dncellassistec@gmail.com>
2026-06-18 19:47:49 -0400

    end of the formLogin interface

---
+++ src/components/form/registerForm.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardContent,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import logo from "../../../public/logo2.png";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export function FormRegiste() {
+  return (
+    <main className="flex w-full items-center justify-center flex-col px-4">
+      <div className="max-w-3xl">
+        <Image
+          alt="logo do app"
+          src={logo}
+          quality={75}
+          priority={true}
+          width={0}
+          height={0}
+          className="sm:w-2xl mb-10"
+        />
+      </div>
+
+      <Card
+        className="w-full max-w-md mx-auto 
+         border border-borderGren"
+      >
+        <CardHeader className="justify-center mt-4 mb-2">
+          <CardTitle>Cadastre sua conta!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-2 mb-4">
+              <Label>Nome completo</Label>
+              <Input
+                className="border border-border "
+                id="email"
+                type="email"
+                placeholder="Nome do usuário..."
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2 mb-4">
+              <Label>Email</Label>
+              <Input
+                className="border border-border "
+                id="email"
+                type="email"
+                placeholder="email@exemple.com..."
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2 mb-4">
+              <Label>Senha</Label>
+              <Input
+                className="border border-border "
+                id="password"
+                type="password"
+                placeholder="*******"
+                required
+              />
+            </div>
+            <div>
+              <Button type="submit" className="flex w-full ">
+                Cadastrar
+              </Button>
+            </div>
+          </form>
+          <CardFooter className="flex flex-col gap-2 items-center justify-center mt-2 border-none ">
+            <div>
+              <Link className="font-bold text-text-blue text-base hover:text-borderGren" href={"/login"}>
+                Faça logim!
+              </Link>
+            </div>
+          </CardFooter>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
