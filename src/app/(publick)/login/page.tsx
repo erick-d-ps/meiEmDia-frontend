a4d4b9afc243c21f9541e1fa11623eff239fa75a
Erick dps <dncellassistec@gmail.com>
2026-06-19 19:35:21 -0400

    change folder name

--- src/app/(publick)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FormLogin } from "@/components/form/loginForm"
-
-export default function Login() {
-  return (
-    <div className="bg-background w-full min-h-screen flex items-center justify-center px-4 py-8">
-      <FormLogin/>
-    </div>
-  );
-}
--- src/app/(publick)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FormRegiste } from "@/components/form/registerForm"
-
-export default function Register() {
-  return (
-    <div  className="bg-background w-full min-h-screen flex items-center justify-center px-4 py-8">
-      <FormRegiste/>
-    </div>
-  );
-}
