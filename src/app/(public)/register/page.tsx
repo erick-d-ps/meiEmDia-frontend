780b88dcb3f173b667fd5b200db05a5a5efbb3d1
Erick dps <dncellassistec@gmail.com>
2026-06-19 19:36:28 -0400

    change folder name

---
+++ src/app/(public)/register/page.tsx
@@ -0,0 +1,9 @@
+import { FormRegiste } from "@/components/form/registerForm"
+
+export default function Register() {
+  return (
+    <div  className="bg-background w-full min-h-screen flex items-center justify-center px-4 py-8">
+      <FormRegiste/>
+    </div>
+  );
+}
