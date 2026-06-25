5c6a12316579b2ac6e32ee9e11d6c6161b4290b4
Erick dps <dncellassistec@gmail.com>
2026-06-25 15:17:54 -0400

    feat: consuming Sidbar desktop

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,24 @@
-import { AuthenticatedUser } from "@/lib/auth"
+import { AuthenticatedUser } from "@/lib/auth";
+import { Sidebar } from "./components/sidbar";
 
 export default async function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode;  
-}){
-   const user = await AuthenticatedUser();
-   //console.log("User logado" ,user)
+  children: React.ReactNode;
+}) {
+  const user = await AuthenticatedUser();
+  //console.log("User logado" ,user)
 
-   return(
-    <div>
-       <main>
-        {children}
-       </main> 
+  return (
+    <div className="flex h-screen overflow-hidden bg-background">
+      <Sidebar/>{/*Desctop*/}
+      <div>
+        <main className="flex-1 overflow-y-auto">
+          <div className="container max-w-full px-4 py-6">
+            {children}
+          </div>
+        </main>
+      </div>
     </div>
-   )
+  );
 }
