40623e3c37b1d1ffc8d7e509b13812313c6c685c
Erick dps <dncellassistec@gmail.com>
2026-07-02 16:56:31 -0400

    feat: consuming components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
+import { InfoMei } from "./components/infoMei";
+import { HistoryButton } from "./components/historyButton";
 
-export default function Dashboard(){
-   return(
-    <div >
-        <h1>PAGINA DASBOARD</h1>
-    </div>
-   ) 
+export default function Dashboard() {
+  return (
+    <main className="min-h-screen bg-background p-4 sm:p-6">
+      <section className="flex items-center flex-col sm:flex-row justify-between gap-2">
+        <div className="flex-1 w-full p-4">
+          <InfoMei />
+        </div>
+        <div className=" flex-1 w-full p-4"></div>
+      </section>
+      <footer className="flex items-center flex-col sm:flex-row justify-between gap-2">
+        <HistoryButton />
+      </footer>
+    </main>
+  );
 }
