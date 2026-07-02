9b017acfb090bd88a55ca455db40b03bf969944e
Erick dps <dncellassistec@gmail.com>
2026-07-02 16:48:53 -0400

    feat:creat infoMei interface

---
+++ src/app/dashboard/components/infoMei.tsx
@@ -0,0 +1,25 @@
+import { ChartNoAxesColumnIncreasing } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import logo from "../../../../public/logo2.png";
+import Image from "next/image";
+
+export function InfoMei() {
+  return (
+    <main className="flex flex-col mx-auto ">
+      <section className="w-full border flex flex-col items-center justify-center bg-bgGreen h-32 rounded-md border-border">
+        <div className=" flex items-center justify-center mb-4">
+          <Image
+            alt="logo do app"
+            src={logo}
+            quality={75}
+            priority={true}
+            width={0}
+            height={0}
+            className="w-3xs"
+          />
+        </div>
+        <span className="text-md">Não ha pendências esse este més. Tudo certo! </span>
+      </section>
+    </main>
+  );
+}
