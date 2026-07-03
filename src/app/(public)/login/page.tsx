3991d9edc4f9ff73c0b3babce99b317aceb12745
Erick dps <dncellassistec@gmail.com>
2026-07-03 16:20:22 -0400

    refactore: check if the token exists

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,9 +1,17 @@
-import { FormLogin } from "@/components/form/loginForm"
+import { redirect } from "next/navigation";
+import { FormLogin } from "@/components/form/loginForm";
+import { getUser } from "@/lib/auth";
 
-export default function Login() {
+export default async function Login() {
+  const user = await getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="bg-background w-full min-h-screen flex items-center justify-center px-4 py-8">
-      <FormLogin/>
+      <FormLogin />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import {redirect} from "next/navigation"
+import { redirect } from "next/navigation";
+import { getUser } from "@/lib/auth";
 
-export default function Login(){
-   return(
-    redirect("/login")
-   )
+export default async function HomePage() {
+  const user = await getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
+  redirect("/login");
 }
