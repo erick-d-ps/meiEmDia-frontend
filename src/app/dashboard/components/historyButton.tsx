969aeebc404575787b663c6ef8c2a3051c477973
Erick dps <dncellassistec@gmail.com>
2026-07-03 20:28:33 -0400

    feat: cursor change

--- src/app/dashboard/components/historyButton.tsx
+++ src/app/dashboard/components/historyButton.tsx
@@ -4,12 +4,12 @@ import { ChartNoAxesColumnIncreasing, Calendar } from "lucide-react";
 export function HistoryButton() {
   return (
     <section className="mx-auto w-full px-4 py-2">
-      <div className="flex flex-col gap-4 lg:flex-row lg:items-stretch">
+      <div className="flex flex-col gap-2 lg:gap-8 lg:flex-row lg:items-stretch">
         <form className="w-full lg:w-1/2">
           <Button
             type="submit"
             variant="outline"
-            className="bg-surface w-full h-15 gap-5"
+            className="bg-surface w-full h-15 gap-5 cursor-pointer"
           >
             <ChartNoAxesColumnIncreasing className="text-text-blue w-6 h-6" />
             <strong>Ver relatorio mensal</strong>
@@ -20,7 +20,7 @@ export function HistoryButton() {
           <Button
             type="submit"
             variant="outline"
-            className="bg-surface w-full h-15 gap-5"
+            className="bg-surface w-full h-15 gap-5 cursor-pointer"
           >
             <Calendar className="text-text-blue w-6 h-6" />
             <strong>Histórico do mês</strong>
