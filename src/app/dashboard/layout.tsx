e8a797a026ea44a3be50a073fa887bc5922d4bc6
Erick dps <dncellassistec@gmail.com>
2026-07-06 19:19:24 -0400

    refactore: change in responsiveness

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,18 +11,18 @@ export default async function DashboardLayout({
   const user = await AuthenticatedUser();
   //console.log("User logado" ,user)
 
-  return (
-    <div className="flex h-screen overflow-hidden bg-background">
-      <Sidebar />
-      {/*Desctop*/}
-      <div className="flex flex-1 flex-col ">
-        {/*Mobile sidebar*/}
-        <MobileSidebar userName={user.name} />
-        <main className="flex-1 overflow-y-auto">
-          <Header userName={user.name} />
-          <div className="container max-w-full px-4 py-6">{children}</div>
-        </main>
+ return (
+  <main className="min-h-screen bg-background caret-transparent lg:flex">
+    <Sidebar />
+
+    <div className="flex min-h-screen flex-1 flex-col">
+      <MobileSidebar userName={user.name} />
+      <Header userName={user.name} />
+
+      <div className="flex-1 px-4 py-4 lg:px-6 lg:py-6">
+        {children}
       </div>
     </div>
-  );
+  </main>
+);
 }
