d66ab879a32f0c14335b411b820d936e8e5dfe8a
Erick dps <dncellassistec@gmail.com>
2026-07-29 18:34:08 -0300

    feat: action to save revenue in the bank

---
+++ src/actions/documentsRevenue.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { apiClient } from "@/lib/api";
+import { RevenueType } from "@/lib/types";
+import { getToken } from "@/lib/auth";
+
+export async function CreateRevenue(formdata: FormData) {
+  try {
+    const token = await getToken();
+    const amount = Number(String(formdata.get("amount")).replace(",", ".")); 
+    const type = formdata.get("type") as string;
+    const date = formdata.get("date") as string;
+    const note = (formdata.get("note") ?? "") as string;
+
+    if(!amount || !type || !date){
+       return {
+        success: false,
+        message: "Preencha todos os campos obrigatórios."
+       } 
+    }
+
+    const data = {
+      amount: amount,
+      type: type,
+      date: date,
+      note: note,
+    };
+
+    await apiClient<RevenueType>("/revenue", {
+      method: "POST",
+      body: JSON.stringify(data),
+      token: token,
+    });
+
+    return { success: true, message: "" };
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        success: false,
+        message: err.message,
+      };
+    }
+  }
+}
